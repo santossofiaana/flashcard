c1c16ad9b038046b661ab498bff48ac8bcdf85bc
santossofiaana <sofia.santos.ana@escola.pr.gov.br>
2025-06-05 10:54:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'matemática',
+    'qual o valor de pi?',
+    'aproximadamente 3,14'
+)
+
+criaCartao(
+    'Geografia',
+    'o que é o el niño?',
+    'o aquecimento anormal das águas do oceano pacífico'
+)
+
+criaCartao(
+    'biologia',
+    'qual o maior órgão do corpo humano?',
+    'a pele'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    ' o que é o verbo to be?',
+    'um grupo de palavras com sentido de indicar o estado de algo ou de alguém'
+)
